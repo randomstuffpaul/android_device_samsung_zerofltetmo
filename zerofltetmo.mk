974381fb292c9af203a5e56c18605a2778ae9fc0
Paul Keith <javelinanddart@gmail.com>
2017-06-13 12:33:51 -0500

    zerofltetmo: Move bt_vendor.conf to common
    Change-Id: Icad408cd16760cb7ed3008572924b10ec6819d22

--- zerofltetmo.mk
+++ zerofltetmo.mk
@@ -4,9 +4,5 @@ LOCAL_PATH := device/samsung/zerofltetmo
 PRODUCT_PACKAGES += \
     init.baseband.rc
 
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/zerofltetmo/zerofltetmo-vendor.mk)
