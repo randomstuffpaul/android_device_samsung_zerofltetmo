a53ba33970a709f4e58ea6e3afed420592bf16cd
Paul Keith <javelinanddart@gmail.com>
2017-06-13 09:58:40 -0500

    zerofltetmo: Remove unused cbd
    * This is in vendor/ now
    
    Change-Id: I3eb29c0d7fe5471124ff5f2440b9427a57953639

--- zerofltetmo.mk
+++ zerofltetmo.mk
@@ -4,11 +4,6 @@ LOCAL_PATH := device/samsung/zerofltetmo
 PRODUCT_PACKAGES += \
     init.baseband.rc
 
-# Radio
-# cpboot-daemon for modem
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
-
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
